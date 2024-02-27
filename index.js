d4fb33194ee929ac025d3fd14809198de4ece103
Sonal Rani <sonalrani@Sonals-MacBook-Air.local>
2024-02-27 21:31:11 +0530

    Integrated MongoDB for data storage in the Facebook Helpdesk application.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var express = require("express"),
-  app = express(),
   path = require("path"),
   bodyParser = require("body-parser"),
   mongoose = require("mongoose"),
   passport = require("passport"),
   User = require("./models/user"),
-  LocalStrategy = require("passport-local");
+  LocalStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose");
 
+var app = express();
 mongoose.connect("mongodb://localhost/login_info");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -26,16 +27,39 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
-  res.render("landing");
+  res.render("signin");
 });
 
 app.post("/", function (req, res) {
-  res.send("Signing you up");
+  req.body.username;
+  req.body.password;
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        return res.render("signin");
+      }
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/home");
+      });
+    }
+  );
 });
 
 app.get("/signin", function (req, res) {
   res.render("signin");
 });
+
+app.get("/signout", function (req, res) {
+  res.render("signout");
+});
+
+app.post("/signout", function (req, res) {
+  res.render("signout/fb");
+});
+
 app.listen(3000, function () {
   console.log("The server has started.");
 });
